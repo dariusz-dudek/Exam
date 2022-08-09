0fe1afa049c9c76144273a0cfc8a22b5fe1e7ee2
Dariusz Dudek <dariusz.dudek@motorolasolutions.com>
2022-08-09 15:10:54 +0200

    add all methods to ReviewController

--- Exam/Exam.API/Controllers/ReviewController.cs
+++ Exam/Exam.API/Controllers/ReviewController.cs
@@ -24,6 +24,11 @@ namespace Exam.API.Controllers
         public async Task<ActionResult<IEnumerable<ReviewDTO>>> Get()
             => Ok(await _reviewService.GetAll());
 
+        /// <summary>
+        /// Add new Review
+        /// </summary>
+        /// <param name="reviewPostDTO">data of new review</param>
+        /// <returns>Review id created from database</returns>
         [HttpPost]
         [Produces(MediaTypeNames.Application.Json)]
         [SwaggerResponse(StatusCodes.Status201Created, Type = typeof(ReviewPostDTO))]
@@ -35,5 +40,31 @@ namespace Exam.API.Controllers
             int id = await _reviewService.CreateNewAsync(reviewPostDTO);
             return Created($"{HttpContext.Request.Path}/{id}", $"new review with id= [{id}] added");
         }
+
+        /// <summary>
+        /// Change data in exiting review
+        /// </summary>
+        /// <param name="reviewPutDTO">Data fresh data</param>
+        /// <returns>Review id updated from database</returns>
+        [HttpPut]
+        [Produces(MediaTypeNames.Application.Json)]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(int))]
+        [SwaggerResponse(StatusCodes.Status409Conflict)]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Put(ReviewPutDTO reviewPutDTO)
+            => Ok($"The changes have passed successfully on actor ID=[{await _reviewService.PutAsync(reviewPutDTO)}]");
+
+        /// <summary>
+        /// Delete review
+        /// </summary>
+        /// <param name="reviewId">Review id to delete</param>
+        /// <returns>Review id removed from Database</returns>
+        [HttpDelete]
+        [Route("{reviewId}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(int))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Delete(int reviewId)
+            => Ok($"Review was removed on ID=[{await _reviewService.DeleteReviewAsync(reviewId)}]");
     }
 }
